a141aae7ba34e58aeb5a34d6180627931ad79ac3
Ramil-Karipov <omil8@yandex.ru>
2023-11-22 13:48:29 +0500

    Task

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,40 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] unknown = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int  sum = 0;
-        for (int element : unknown) {
-            if (element % 2 == 0) {
-                sum = element + 1;
-                System.out.println(sum);
+        int[] resultOut = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
+        int[] resultln = new int[3];
+        for (int i = 0; i < resultOut.length; i++) {
+            resultln[i] = resultOut[i + 3];
+            System.out.println(Arrays.toString(total(resultln)));
             }
+    }
 
-
+    public static int[] total(int[] result) {
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] % 2 == 0) {
+                result[i]++;
+            }
         }
+        return result;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
